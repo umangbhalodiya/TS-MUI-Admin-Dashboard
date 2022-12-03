d1f6b1d2c721bf3c01250d9328702cd157581f7f
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-12-03 21:06:25 +0530

    cmnt

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,16 +1,3 @@
-/* eslint-disable */
-/* @ts-nocheck */
-
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
- 
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.

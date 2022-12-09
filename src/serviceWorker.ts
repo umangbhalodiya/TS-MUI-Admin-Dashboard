803b670de904bce403ad47147f986804289456f3
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-12-09 21:00:28 +0530

    cmnt

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -27,11 +27,7 @@ export function register(config?: Config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
+         checkValidServiceWorker(swUrl, config);
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
@@ -39,8 +35,7 @@ export function register(config?: Config) {
           );
         });
       } else {
-        // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config);
+         registerValidSW(swUrl, config);
       }
     });
   }
